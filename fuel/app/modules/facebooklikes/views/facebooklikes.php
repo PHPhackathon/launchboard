61e60696b1c52cc0aeb9dc3b5903a25f4ccd7b07
Martijn Croonen <martijn.croonen@mail.phl.be>
2011-05-28 05:17:11 +0200

    Facebook likes modules

--- fuel/app/modules/facebooklikes/views/facebooklikes.php
+++ fuel/app/modules/facebooklikes/views/facebooklikes.php
@@ -1,16 +1,4 @@
-<div id="module_analytics" class="box h_one w_four">
-    <select id="select_analytics">
-    <?php
-    var_dump($aUrls);
-        foreach($aUrls as $sUrl => $sTableId) {
-    ?>
-            <option value="<?php echo $sUrl; ?>"><?php echo $sUrl; ?></option>
-    <?php
-        } // end foreach
-    ?>
-    </select>
-    <a href="/analytics?reset=true">Reset</a>
-    <div id="analytics_chart">
-        
-    </div>
+<div id="module_facebooklikes" class="box h_one w_two">
+    <p class="desc">Facebook likes for <em><?php echo $sName ?></em></p>
+    <p class="likes"><?php echo $nLikes ?></p>
 </div>
