a52023e4c7a257e29aea1a8906fd0a4083272d60
Stijn Janssen <me@stijnjanssen.be>
2011-05-28 06:09:50 +0200

    add UI for facebooklikes

--- fuel/app/modules/facebooklikes/views/facebooklikes.php
+++ fuel/app/modules/facebooklikes/views/facebooklikes.php
@@ -1,4 +1,7 @@
+<style>
+    .likes { font-size:100px; line-height: 150px; text-align: center; }
+</style>
 <div id="module_facebooklikes" class="h_one w_two">
-    <p class="desc">Facebook likes for <em><?php echo $sName ?></em></p>
+    <span class="heading">Facebook: <?php echo $sName ?></span>
     <p class="likes"><?php echo $nLikes ?></p>
 </div>
